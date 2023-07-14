e8b198845cbba8e0a63d3130c1524bacfcf7eff9
alexander muñoz <alexander12345my@gmail.com>
2023-07-14 16:15:25 -0500

    server

---
+++ Server/src/index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const characters = require("./Utils/data");
+const PORT = 3001;
+http
+  .createServer((req, res) => {
+    const { url } = req;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    if (url.includes("rickandmorty/character")) {
+      let urlId = url.split("/").pop();
+      let found = characters.find(
+        (character) => character.id === Number(urlId)
+      );
+
+      res.writeHead(200, {'content-type': 'application/json'})
+      res.end(JSON.stringify(found));
+    }
+    return
+  })
+  .listen(PORT);
